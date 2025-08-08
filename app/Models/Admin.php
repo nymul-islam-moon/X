7d77023be8f8c27aa9eb019cb83139aafdd9ed84
Nymul Islam <nymulislam.dev@gmail.com>
2025-08-08 19:33:06 +0600

    multi auth working

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,15 +2,15 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class Admin extends Model
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+    /** @use HasFactory<\Database\Factories\AdminFactory> */
     use HasFactory, Notifiable;
+  
 
     /**
      * The attributes that are mass assignable.
